b888289188bfc90c77269826be6c83526e276492
developerhinakhan <developerhinakhan@gmail.com>
2026-05-30 18:19:06 +0500

    feat: add user model

---
+++ backend/app/modules/users/model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, default="cashier")
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+    contact_no = Column(String, nullable=True)
+    address = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
